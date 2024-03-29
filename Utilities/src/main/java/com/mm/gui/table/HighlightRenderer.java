a8a8c6ed6f798aafa4ee59510e899e7c5d1fd90b
SwingGuy1024 <SwingGuy1024@yahoo.com>
2024-03-29 08:39:07 -0700

    Changed Low & High to Start & End Bug Fixes. It now works perfectly in my test case. Added HighlightMode enum New constructors take the HighlightMode enum. New setters Removed diagnostics. TODOs completed Javadocs

--- Utilities/src/main/java/com/mm/gui/table/HighlightRenderer.java
+++ Utilities/src/main/java/com/mm/gui/table/HighlightRenderer.java
@@ -13,79 +13,125 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
+ * <p>Renderer to highlight selected text. This works by wrapping the text inside html tags, and decorating spans that 
+ * contain text to be decorated. The choice of text to decorate is determined by an external class that implements
+ * the HighlightRangeSource interface. So, for example, to connect to a spell checker, you may write a method that
+ * takes examines the text in the field for spelling checkers and returns a List of TextRange instances, each of
+ * which has a start index (inclusive) and an end index (exclusive). The renderer calls this method as it renders
+ * the cell, and highlights the text ranges that it gets back.</p>
+ * <p>The highlightStyle property is a String that would appear in an html style attribute. For example, to
+ * set color the highlighted text in magenta, you could call the setter like this:</p>
+ * <pre>
+ *   HighlightRenderer myRenderer = new HighlightRenderer();
+ *   myRenderer.setHighlightStyle("color:red;");
+ * </pre>
+ * <p>This would produce a html String that would render a misspelled word like this:</p>
+ * <pre>{@code   <html>This has a <span style="color:red">mispelled</span> word.</html> }</pre>
  * <p>Created by IntelliJ IDEA.</p>
  * <p>Date: 3/28/24</p>
  * <p>Time: 9:24 AM</p>
  * <p>@author Miguel Muñoz</p>
  */
+@SuppressWarnings("unused")
 public class HighlightRenderer extends DefaultTableCellRenderer {
-  
+
+  /**
+   * <p>Interface to specify a range of text to get highlighted.</p>
+   * <p>As with the {@code String.substring(...)} method, start is inclusive,
+   * and end is exclusive.</p>
+   */
   public interface TextRange {
-    int getLow();
-    int getHigh();
+    int getStart();
+    int getEnd();
   }
 
+  /**
+   * <p>Default implementation of TextRange. This class is immutable and sortable.
+   * In addition to its constructor that takes start and end values, it
+   * also has a static {@code of()} method, that takes a start index and a
+   * length.</p>
+   */
   public static class TextRangeImpl implements TextRange, Comparable<TextRange> {
-    private final int lowInclusive;
-    private final int highExclusive;
+    private final int startInclusive;
+    private final int endExclusive;
     
-    public TextRangeImpl(int lowInclusive, int highExclusive) {
-      if (lowInclusive > highExclusive) {
-        this.lowInclusive = highExclusive;
-        this.highExclusive = lowInclusive;
+    public TextRangeImpl(int startInclusive, int endExclusive) {
+      if (startInclusive <0) {
+        throw new IllegalArgumentException(String.format("Negative Start: %d", startInclusive));
+      }
+      if (endExclusive < 0) {
+        throw new IllegalArgumentException(String.format("Negative end: %d", endExclusive));
+      }
+      if (startInclusive == endExclusive) {
+        throw new IllegalArgumentException(String.format("Start equals End: %d", startInclusive));
+      }
+      if (startInclusive > endExclusive) {
+        this.startInclusive = endExclusive;
+        this.endExclusive = startInclusive;
       } else {
-        this.lowInclusive = lowInclusive;
-        this.highExclusive = highExclusive;
+        this.startInclusive = startInclusive;
+        this.endExclusive = endExclusive;
       }
     }
 
     @Override
-    public int getLow() {
-      return lowInclusive;
+    public int getStart() {
+      return startInclusive;
     }
 
     @Override
-    public int getHigh() {
-      return highExclusive;
+    public int getEnd() {
+      return endExclusive;
     }
 
     @Override
     public int compareTo(@NotNull HighlightRenderer.TextRange that) {
-      return Integer.compare(this.getLow(), that.getLow());
+      return Integer.compare(this.getStart(), that.getStart());
     }
 
     @Override
     public int hashCode() {
       // Adapted from java.awt.Dimension
-      int sum = lowInclusive + highExclusive;
-      return ((sum * (sum + 1)) / 2) + lowInclusive;
+      int sum = startInclusive + endExclusive;
+      return ((sum * (sum + 1)) / 2) + startInclusive;
     }
 
     @Override
     public boolean equals(Object obj) {
       if (obj instanceof TextRange) {
         TextRange that = (TextRange) obj;
-        return (this.lowInclusive == that.getLow()) && (this.highExclusive == that.getHigh());
+        return (this.startInclusive == that.getStart()) && (this.endExclusive == that.getEnd());
       }
       return false;
     }
 
     @Override
     public String toString() {
-      return String.format("(%s -> %s)", lowInclusive, highExclusive);
+      return String.format("(%s -> %s)", startInclusive, endExclusive);
     }
 
-    public static TextRange of(int lowInclusive, int width) {
-      return new TextRangeImpl(lowInclusive, lowInclusive + width);
+    public static TextRange of(int start, int width) {
+      if (width <= 0) {
+        throw new IllegalArgumentException(String.format("Negative or zero width: %d", width));
+      }
+      return new TextRangeImpl(start, start + width);
     }
   }
-  
+
+  /**
+   * <p>This interfaces to whatever external systems will determine what text gets highlighted.</p>
+   * <p>Implementing classes must meet these conditions.</p>
+   * <p>1. Ranges must not overlap.</p>
+   * <p>2. Ranges must be sorted in ascending order</p>
+   */
   @FunctionalInterface
   public interface HighlightRangeSource {
     /**
-     * Return non-overlapping text ranges, in increasing order, describing text that should get highlighted.
+7     * <p>Return a list of non-overlapping {@code TextRange} instances, in
+     * increasing order, specifying the text that should get highlighted.</p>
      * @param text The text to inspect for possible highlights
-     * @return an ordered array, in increasing order, of text ranges to highlight
+     * @return An ordered array, in increasing order, of non-overlapping text
+     * ranges to highlight.
      */
     List<TextRange> getOrderedTextRanges(String text);
   }
@@ -93,21 +139,45 @@ public class HighlightRenderer extends DefaultTableCellRenderer {
   
   private @Nullable HighlightRangeSource highlightRangeSource;
   private String highlightStyle; // example: color:red;
-  
-  public HighlightRenderer() {
-    this(null, "");
-  }
 
+  /**
+   * <p>Instantiate a HighlightRenderer with no highlighting mechanism
+   * specified. The {@code HighlightRangeSource} object may be set later.
+   * The highlight style defaults to {@code color:red;}</p>
+   */
+  public HighlightRenderer() {
+    this(null, "color:red;");
+  }
+  
+  public HighlightRenderer(String highlightStyle) {
+    super();
+    this.highlightStyle = highlightStyle;
+  }
+  
+  public HighlightRenderer(HighlightMode highlightMode) {
+    super();
+    this.highlightStyle = highlightMode.getStyle();
+  }
+  
   public HighlightRenderer(@Nullable HighlightRangeSource highlightRangeSource, String highlightStyle) {
     super();
     this.highlightRangeSource = highlightRangeSource;
     this.highlightStyle = highlightStyle;
   }
 
+  public HighlightRenderer(@Nullable HighlightRangeSource highlightRangeSource, HighlightMode highlightMode) {
+    super();
+    this.highlightRangeSource = highlightRangeSource;
+    this.highlightStyle = highlightMode.getStyle();
+  }
+
   @Override
   public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
 
     final JLabel renderer = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+    
+    // Most of these values are obvious, but some were determined by stepping through the method when a 
+    // paint() method was calling it. 
     String displayText = SwingUtilities.layoutCompoundLabel(renderer,
         renderer.getFontMetrics(renderer.getFont()),
         renderer.getText(),
@@ -117,34 +187,29 @@ public class HighlightRenderer extends DefaultTableCellRenderer {
         renderer.getVerticalTextPosition(),
         renderer.getHorizontalTextPosition(),
         prepareRect(table.getCellRect(row, column, false), renderer.getInsets()),
-        new Rectangle(), // TODO: Figure out how to calculate the iconRectangle.
-        new Rectangle(), // TODO: Figure out how to calculate the textRectangle. (both are empty with no icon
+        new Rectangle(),
+        new Rectangle(),
         renderer.getIconTextGap()
         );
-    if (column == 1) {
-      System.out.printf("displayText ends with %s at row %2d%n", getLastThree(displayText), row); // NON-NLS
-    }
     String fullText = value.toString();
-    boolean textClipped = !fullText.equals(displayText);
-    int maxDisplayLength = textClipped ? (displayText.length() - 3) : fullText.length();
+
+    // 3 is for the three dots at the end.
+    int maxDisplayLength = fullText.equals(displayText) ? fullText.length() : (displayText.length() - 3);
+
     if (highlightRangeSource != null) {
-      System.out.printf("Border: %s%n", renderer.getBorder());
-      System.out.printf("Insets: %s%n", renderer.getInsets()); // NON-NLS
       List<TextRange> highlightRanges = highlightRangeSource.getOrderedTextRanges(fullText);
-      int previousHigh = 0;
-      for (TextRange range: highlightRanges) {
-        assert range.getLow() < range.getHigh();
-        assert range.getLow() >= 0;
-        assert range.getHigh() >= 0;
-        assert range.getLow() >= previousHigh;
-        previousHigh = range.getHigh();
-      }
       if (!highlightRanges.isEmpty()) {
         StringBuilder builder = new StringBuilder();
         // Text below this number has been completely highlighted.
         int indexCompleted = 0;
+        int previousHigh = 0;
         for (TextRange textRange: highlightRanges) {
-          final int start = textRange.getLow();
+          assert textRange.getStart() < textRange.getEnd();
+          assert textRange.getStart() >= 0;
+          assert textRange.getStart() >= previousHigh;
+          previousHigh = textRange.getEnd();
+
+          final int start = textRange.getStart();
           assert start >= 0 : String.format("Start = %d < 0", start);
           if (start < maxDisplayLength) {
             builder
@@ -152,13 +217,13 @@ public class HighlightRenderer extends DefaultTableCellRenderer {
                 .append("<span style=\"")
                 .append(highlightStyle)
                 .append("\">");
-            int end = textRange.getHigh();
+            int end = textRange.getEnd();
             if (end > maxDisplayLength) {
-              System.out.printf("range to find %d: %d - %d in <%s>%n", displayText.length(), start, end, displayText); // NON-NLS
               builder
                   .append(displayText.substring(start))
                   .append("</span>");
-              indexCompleted = maxDisplayLength;
+              indexCompleted = displayText.length();
+              break;
             } else {
               builder
                   .append(displayText, start, end)
@@ -166,9 +231,8 @@ public class HighlightRenderer extends DefaultTableCellRenderer {
               indexCompleted = end;
             }
           } else {
-            if (maxDisplayLength < indexCompleted) {
-              System.out.printf("Error: %d < %d%n", maxDisplayLength, indexCompleted); // NON-NLS
-            }
+            assert maxDisplayLength >= indexCompleted: 
+                String.format("maxDisplayLength < indexCompleted: %d < %d", maxDisplayLength, indexCompleted);
             builder
                 .append(fullText, indexCompleted, maxDisplayLength)
                 .append("<span style=\"")
@@ -179,20 +243,20 @@ public class HighlightRenderer extends DefaultTableCellRenderer {
             indexCompleted = displayText.length();
             break;
           }
-        }
-        if (indexCompleted < maxDisplayLength) {
+        } // end for loop
+        if (indexCompleted <= maxDisplayLength) {
           builder.append(displayText.substring(indexCompleted));
-        }
+        } 
         builder
             .insert(0, "<html>")
             .append("</html>");
         renderer.setText(builder.toString());
-        System.out.printf("Using      %s%nto display %s%nfrom       %s%n", builder, displayText, fullText); // NON-NLS
+//        System.out.printf("%nUsing      %s%nto display %s%nfrom       %s%n", builder, displayText, fullText); // NON-NLS
       }
     }
     return renderer;
-    
   }
+
   private static Rectangle prepareRect(Rectangle rectangle, Insets insets) {
     return new Rectangle(
         insets.left,
@@ -202,19 +266,40 @@ public class HighlightRenderer extends DefaultTableCellRenderer {
     );
   }
   
-  private String getLastThree(String text) {
-    if (text.length() > 2) {
-      return text.substring((text.length()-3));
-    }
-    return text;
-  }
-
   public @Nullable HighlightRangeSource getHighlightRangeSource() {
     return highlightRangeSource;
+  }
+  
+  public void setHighlightRangeSource(HighlightRangeSource highlightRangeSource) {
+    this.highlightRangeSource = highlightRangeSource;
+  }
+
+  /**
+   * <p>Sets the highlightStyle. This is a String that would appear in an html style attribute</p>
+   * @param highlightStyle The highlight style.
+   */
+  public void setHighlightStyle(String highlightStyle) {
+    this.highlightStyle = highlightStyle;
   }
 
   public void setHighlightRangeSource(@Nullable HighlightRangeSource highlightRangeSource, String highlightStyle) {
     this.highlightRangeSource = highlightRangeSource;
     this.highlightStyle = highlightStyle;
+  }
+  
+  public enum HighlightMode {
+    RedText("color:red;"),
+    RedUnderlinedText("text-decoration:underline; color:#ff0000;"),
+    UnderlinedText("text-decoration: underline;"),
+    PinkHighlight("background-color: #ffaaaa;"),
+    YellowHighlight("background-color: #f5d51b;"),
+    BlueHighlight("background-color: #aaddff;"),
+    ;
+    private final String style;
+    HighlightMode(String style) {
+      this.style = style;
+    }
+    
+    public String getStyle() { return style; }
   }
 }
